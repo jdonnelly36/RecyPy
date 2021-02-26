2f316cb604d744bf86861f33692910c68398b559
sallertonc <spencer.campbell1@maine.edu>
2021-02-26 12:49:19 -0500

    user accounts

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', function() {
+        return view('home');
+    });
+});
