ddd1f136c716f7df368653247e492b213c1d51f3
sallertonc <spencer.campbell1@maine.edu>
2021-03-01 13:46:00 -0500

    some more examples on pages

---
+++ laravel/resources/views/base.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+    // INCLUDED ON EVERY PAGE
+
+?>
+<head>
+    {{--    JQUERY--}}
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+{{--    Semantic UI--}}
+    <script type="text/javascript" src="{{asset('addons/SemanticUI/semantic.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('addons/SemanticUI/semantic.min.css')}}">
+
+{{--    JS Includes--}}
+    <script type="text/javascript" src="{{asset('scripts/js/includes.js')}}"></script>
+</head>
+<script>
+    console.log('base loaded!')
+</script>
